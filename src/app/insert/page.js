5c9aa059fb5542660f0a6770954a818044528cc0
wwg98 <wwwg98@gmail.com>
2026-08-06 09:51:09 +0900

    feat: implement user authentication and thumbnail upload functionality

--- src/app/insert/page.js
+++ src/app/insert/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { createClient } from "@/utils/supabase/client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Insert() {
@@ -22,6 +22,20 @@ export default function Insert() {
   });
 
   const [thumbnail, setThumbnail] = useState(null);
+  const [user, setUser] = useState(null);
+  const [authForm, setAuthform] = useState({
+    email: "",
+    password: "",
+  });
+
+  useEffect(() => {
+    (async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+    })();
+  }, [supabase.auth]);
 
   async function insertData(e) {
     e.preventDefault();
@@ -32,7 +46,11 @@ export default function Insert() {
       console.log("데이터 입력 성공");
       router.push("/");
     }
+    if (thumbnail) {
+      await uploadThumbnail(thumbnail);
+    }
   }
+
   const handleChange = (e) => {
     const { name, value } = e.target;
 
@@ -45,6 +63,47 @@ export default function Insert() {
   const handleFileChange = (e) => {
     setThumbnail(e.target.files[0]);
   };
+
+  async function uploadThumbnail(file) {
+    const { data, error } = await supabase.storage
+      .from("portfolio")
+      .upload(`thumbnail/${file.name}`, file);
+    if (error) {
+      console.error("썸네일 업로드 실패:", error);
+    } else {
+      // Handle success
+      console.log("썸네일 업로드 성공:", data);
+    }
+  }
+
+  if (!user) {
+    return (
+      <div className="about_content shadow">
+        <h2>관리자 로그인</h2>
+        <div className="contact_form">
+          <form action="">
+            <p className="field">
+              <label htmlFor="email">이메일</label>
+              <input type="email" id="email" name="email" placeholder="email" required />
+            </p>
+            <p className="field">
+              <label htmlFor="password">비밀번호</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="password"
+                required
+              />
+            </p>
+            <p className="submit">
+              <input type="submit" className="primary-btn" value="등록" />
+            </p>
+          </form>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="about_content shadow">
